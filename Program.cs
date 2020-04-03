fa66db2a7fd8bdb10c1ed1acb3ebe71d5f824c47
Jakub Falis <jfmysc2@gmail.com>
2020-04-03 14:01:59 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ReadATextFile
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Podaj scieżkę dostępu do pliku: "); //C:\Users\Kuba\Desktop\menu.sql
+            string textFile = Console.ReadLine();
+
+            try
+            {
+                if (File.Exists(textFile))
+                {
+
+                    // Read file using StreamReader. Reads file line by line  
+                    using (StreamReader file = new StreamReader(textFile))
+                    {
+                        int counter = 0;
+                        string line;
+
+
+
+                        while ((line = file.ReadLine()) != null)
+                        {
+
+
+
+
+                            if (string.IsNullOrWhiteSpace(line) == true)
+                            {
+                                continue;
+                            }
+                            line = Regex.Replace(line, "--.*", "");
+
+                            Console.WriteLine(line);
+                            counter++;
+                        }
+                        file.Close();
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
